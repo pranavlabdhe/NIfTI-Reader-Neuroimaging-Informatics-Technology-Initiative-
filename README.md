b626ab809ef325c1740c58b98a0e6def5dfc3dbe
pranav <pranav@pranavs-MacBook-Air.local>
2024-06-20 16:09:56 +0530

    CodeCleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-Application Demo 
-Drive Link : https://drive.google.com/file/d/1jZrgtk8165Q2DfPEXwRxJUPBDkqys7UW/view?usp=sharing
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Image1 from './Component/NiftiViewer'
 import NiftiViewer from './Component/NiftiViewer';
-import TicTac from './Component/TicTac';
 function App() {
   return (
     <div className="App">
